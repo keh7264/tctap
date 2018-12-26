bfc7ba76e01c47063d942ed3d70fced59059c25a
ehkim <keh7264@gmail.com>
2018-12-26 13:13:38 +0900

    create faculty lists

--- src/app/faculties/board-list/board-list.component.ts
+++ src/app/faculties/board-list/board-list.component.ts
@@ -1,3 +1,4 @@
+import { httpService } from './../../shared/http.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-list.component.scss']
 })
 export class BoardListComponent implements OnInit {
+  filteredList: any;
 
-  constructor() { }
+  constructor(private http: httpService) {}
 
   ngOnInit() {
+    this.getData();
   }
 
+  getData() {
+    this.http
+      .query('get_faculty_list', { group_id: 10, annual: '2018' })
+      .subscribe(response => {
+        this.filteredList = response.filteredList;
+        console.log(response);
+      });
+  }
 }
--- src/app/faculties/list/list.component.ts
+++ src/app/faculties/list/list.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute } from '@angular/router';
+import { httpService } from './../../shared/http.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  filteredList: any;
+  groupId: string;
+  constructor(private http: httpService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.groupId = this.route.snapshot.paramMap.get('id');
+    this.getData();
   }
 
+  getData() {
+    this.http
+      .query('get_faculty_list', { group_id: this.groupId, annual: '2018' })
+      .subscribe(response => {
+        this.filteredList = response.filteredList;
+        console.log(response);
+      });
+  }
 }
